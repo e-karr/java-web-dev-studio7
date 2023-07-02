0e79e1bad82820fbdb667bd6705ae4119063c326
Elizabeth Karr <e.karr93@icloud.com>
2023-07-02 14:35:00 -0500

    start studio

---
+++ src/org/launchcode/studio7/BaseDisc.java
@@ -0,0 +1,12 @@
+package org.launchcode.studio7;
+
+public abstract class BaseDisc {
+    //fields
+    //name, capacity, contents, disc type
+
+    //methods
+    //write data
+    //read data
+    //pause
+    //play
+}
--- src/org/launchcode/studio7/CD.java
+++ src/org/launchcode/studio7/CD.java
@@ -1,7 +1,7 @@
 package org.launchcode.studio7;
 
 public class CD {
-
+    //skip song
     // TODO: Implement your custom interface.
 
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
